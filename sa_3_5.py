db1e116425aa8610753bb39488aa920214b4b6a2
Dragon' Dave McKee <dave.mckee@gmail.com>
2019-06-10 17:20:50 +0100

    SA 3.5 preview

--- sa_3_5.py
+++ sa_3_5.py
@@ -1,14 +1,15 @@
 from databaker.framework import *
 import sa_walk
+import re
 
-files = [x for x in sa_walk.SA_files() if x.table=="3.3"]
+files = [x for x in sa_walk.SA_files() if x.table=="3.5"]
 
 for year, _, filename in files:
     YEAR = int(year)
     # path=f"data/raw/South Africa Staff Tables_raw/{YEAR} Staff tables for Universities/{YEAR} Table 3.3 for Universities.xls"
 
     for tab in loadxlstabs(filename, "*", verbose = False):
-        institution = tab.filter_one(lambda cell: cell.x==0 and cell.y==1).value
+        institution = tab.filter_one(lambda cell: cell.x==0 and cell.y==0).value # Note: different to 3_3! Yuck!
         if not institution:  # Further tidying required -- INSTITUTION: H09 (UNIVERSITY OF LIMPOPO)
             institution = "National"
         else:
@@ -16,19 +17,21 @@ for year, _, filename in files:
         print (institution)
 
 
-        race = tab.filter("WHITE") | tab.filter("COLOURED") | tab.filter("INDIAN") | tab.filter("AFRICAN") | tab.filter("ALL OTHER") | tab.filter("TOTAL")
-        assert len(race) == 6
-        genders = tab.filter("MALE") | tab.filter("FEMALE") | tab.filter("UNKNOWN") | tab.filter("TOTAL")
-        assert len(genders) == 4
-        profcat = tab.filter_one("PERSONNEL CATEGORY").fill(DOWN)
-        observations = profcat.fill(RIGHT).is_not_blank()
+        jobtitles = tab.filter("NOTES").fill(RIGHT).is_not_blank()
+        genders = tab.filter("M") | tab.filter("F") | tab.filter("U") | tab.filter("T")
+        try:
+            ages = tab.filter_one(re.compile("(?:Age|AGE)")).fill(DOWN)
+        except:
+            print (filename)
+            raise
+        observations = ages.fill(RIGHT).is_not_blank()
 
         dimensions = [
             HDimConst(DATAMARKER, 0),
             HDimConst("Institution", institution),
             HDim(genders, "Gender", DIRECTLY, ABOVE),
-            HDim(race, "Race", DIRECTLY, ABOVE),
-            HDim(profcat, "Profession", DIRECTLY, LEFT),
+            HDim(ages, "Age", DIRECTLY, LEFT),
+            HDim(jobtitles, "Profession", CLOSEST, LEFT),
             HDimConst(TIME, YEAR)
         ]
 
